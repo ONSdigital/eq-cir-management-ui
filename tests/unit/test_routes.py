2667241536edf4a2f9b3ffb1a6bcc85c3f1ae7e9
Paul-Joel <paul.joel@ons.gov.uk>
2025-04-23 15:33:14 +0100

    Resolve test and lint combination issues

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -6,7 +6,7 @@ from eq_cir_management_ui.app import create_app
 
 
 @pytest.fixture
-def client():
+def test_client():
     """Creates and configures a test client for the application.
 
     This function initializes the application in testing mode and provides
@@ -18,17 +18,17 @@ def client():
     app = create_app()
     app.config["TESTING"] = True
 
-    with app.test_client() as test_client:
-        yield test_client
+    with app.test_client() as client:
+        yield client
 
 
-def test_index_route(client):
+def test_index_route(test_client):  # pylint: disable=redefined-outer-name
     """Test the index route of the application.
 
     This test sends a GET request to the root URL ("/") using the test client
     and verifies that the response has a status code of 200 and contains
     the expected content "Hello, World!" in the response data.
     """
-    response = client.get("/")
+    response = test_client.get("/")
     assert response.status_code == 200
     assert b"Hello, World!" in response.data
