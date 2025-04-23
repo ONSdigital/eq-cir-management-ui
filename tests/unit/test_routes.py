74596cd6cd438dcbc59b8205125eb27adba7206f
Paul-Joel <paul.joel@ons.gov.uk>
2025-04-23 15:04:10 +0100

    Fix test

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -22,13 +22,13 @@ def client():
         yield test_client
 
 
-def test_index_route(test_client):
+def test_index_route(client):
     """Test the index route of the application.
 
     This test sends a GET request to the root URL ("/") using the test client
     and verifies that the response has a status code of 200 and contains
     the expected content "Hello, World!" in the response data.
     """
-    response = test_client.get("/")
+    response = client.get("/")
     assert response.status_code == 200
     assert b"Hello, World!" in response.data
