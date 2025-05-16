da23911fcc501bbf2d40f54f3a940e1ee7f1be78
Paul-Joel <paul.joel@ons.gov.uk>
2025-05-16 15:00:47 +0100

    Update app.py for debug setting

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ app = create_app(config)
 if __name__ == "__main__":
     host = os.environ.get("HOST", "127.0.0.1")
     port = int(os.environ.get("PORT", 5100))
-    app.run(host=host, port=port, debug=True)
+    debug_mode = os.environ.get("FLASK_ENV") == "development"
+    app.run(host=host, port=port, debug=debug_mode)
